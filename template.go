a2be8e0fca77cd3d77779294e9265a0a583f2ea3
iman tung <iman.tung@gmail.com>
2020-02-02 08:46:13 +0700

    create isTrue test from example function + tidy up

--- template.go
+++ template.go
@@ -34,6 +34,16 @@ func New() *Template {
 	}
 }
 
+// Must is a helper that wraps a call to a function returning (*Template, error)
+// and panics if the error is non-nil. It is intended for use in variable
+// initializations such as
+func Must(t *Template, err error) *Template {
+	if err != nil {
+		panic(err)
+	}
+	return t
+}
+
 // Parse the template
 func (tpl *Template) Parse(source string) (*Template, error) {
 	var program *ast.Program
--- utils_test.go
+++ utils_test.go
@@ -1,51 +1,72 @@
-package mario
+package mario_test
 
-import "fmt"
+import (
+	"testing"
 
-func ExampleIsTrue() {
-	output := "Empty array: " + Str(IsTrue([0]string{})) + "\n"
-	output += "Non empty array: " + Str(IsTrue([1]string{"foo"})) + "\n"
+	"github.com/imantung/mario"
+	"github.com/stretchr/testify/require"
+)
 
-	output += "Empty slice: " + Str(IsTrue([]string{})) + "\n"
-	output += "Non empty slice: " + Str(IsTrue([]string{"foo"})) + "\n"
+func TestIsTrue(t *testing.T) {
+	testcases := []struct {
+		obj   interface{}
+		truth bool
+	}{
+		{[0]string{}, false},
+		{[1]string{"foo"}, true},
+		{[]string{}, false},
+		{[]string{"foo"}, true},
+		{map[string]string{}, false},
+		{map[string]string{"foo": "bar"}, true},
+		{"", false},
+		{"foo", true},
+		{true, true},
+		{false, false},
+		{0, false},
+		{10, true},
+		{-10, true},
+		{0.0, false},
+		{10.0, true},
+		{struct{}{}, true},
+		{nil, false},
+	}
 
-	output += "Empty map: " + Str(IsTrue(map[string]string{})) + "\n"
-	output += "Non empty map: " + Str(IsTrue(map[string]string{"foo": "bar"})) + "\n"
+	for _, tt := range testcases {
+		require.Equal(t, tt.truth, mario.IsTrue(tt.obj), tt.obj)
+	}
+}
 
-	output += "Empty string: " + Str(IsTrue("")) + "\n"
-	output += "Non empty string: " + Str(IsTrue("foo")) + "\n"
+func TestStr(t *testing.T) {
+	t.Parallel()
+	testcases := []struct {
+		input  interface{}
+		output string
+	}{
+		{"foo", "foo"},
+		{true, "true"},
+		{false, "false"},
+		{25, "25"},
+		{25.75, "25.75"},
+		{nil, ""},
+		{[]string{"foo", "bar"}, "foobar"},
+		{[]interface{}{"foo", "bar"}, "foobar"},
+		{[]bool{true, false}, "truefalse"},
+	}
 
-	output += "true bool: " + Str(IsTrue(true)) + "\n"
-	output += "false bool: " + Str(IsTrue(false)) + "\n"
+	for _, tt := range testcases {
+		require.Equal(t, tt.output, mario.Str(tt.input))
+	}
+}
 
-	output += "0 integer: " + Str(IsTrue(0)) + "\n"
-	output += "positive integer: " + Str(IsTrue(10)) + "\n"
-	output += "negative integer: " + Str(IsTrue(-10)) + "\n"
+func TestSafeString(t *testing.T) {
+	tpl, err := mario.New().
+		WithHelperFunc("em", func() mario.SafeString {
+			return mario.SafeString("<em>FOO BAR</em>")
+		}).
+		Parse("{{em}}")
+	require.NoError(t, err)
 
-	output += "0 float: " + Str(IsTrue(0.0)) + "\n"
-	output += "positive float: " + Str(IsTrue(10.0)) + "\n"
-	output += "negative integer: " + Str(IsTrue(-10.0)) + "\n"
-
-	output += "struct: " + Str(IsTrue(struct{}{})) + "\n"
-	output += "nil: " + Str(IsTrue(nil)) + "\n"
-
-	fmt.Println(output)
-	// Output: Empty array: false
-	// Non empty array: true
-	// Empty slice: false
-	// Non empty slice: true
-	// Empty map: false
-	// Non empty map: true
-	// Empty string: false
-	// Non empty string: true
-	// true bool: true
-	// false bool: false
-	// 0 integer: false
-	// positive integer: true
-	// negative integer: true
-	// 0 float: false
-	// positive float: true
-	// negative integer: true
-	// struct: true
-	// nil: false
+	output, err := tpl.Execute(nil)
+	require.NoError(t, err)
+	require.Equal(t, `<em>FOO BAR</em>`, output)
 }
